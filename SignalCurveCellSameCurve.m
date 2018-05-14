d51ba7c5621dc29616f9b7c292ada0e8f27988e8
MacielBruno <brunomacielm@gmail.com>
2018-05-14 15:38:25 -0300

    Add files via upload

--- SignalCurveCellSameCurve.m
+++ SignalCurveCellSameCurve.m
@@ -13,13 +13,50 @@ textArray(3) = java.lang.String('three');
 
 for i = 1:numberOfCurves
     [X(i).plot,Y(i).plot] = XandYGeneratorSameCurve(Signal(i).channel, Nbits, idle, Tbit);
-    
-% Esta região do software vai definir quantos bits de um pacote são
-% representados por um nome, isto é, digamos que tenhamos um pacote dado
-% por [00001111] e os quatro primeiros bits são chamados de "ZERO" e o
-% resto chamado de "UM", logo, associaremos o vetor ["ZERO"; "UM"] a [4,4].
-% Faremos tal associação usando o valor .type de cada canal.
-    
+end
+%text = getCertainComponents(X, typeOfInput, stringArray);
+
+for i = 1:numberOfCurves
+    Y(i).plot = Y(i).plot + (i-1)*2;
+end
+ylim([-1 2]);
+colors = [ [1 0 0]; [0 1 0]; [0 0 1]; [1 1 0];[1 0 1];[0 1 1];[0 0 0]];
+
+% Queremos colocar todos os valores de Y em uma mesma matriz para plotá-los
+% ao mesmo tempo, porém o comprimento dos vetores são diferentes, logo
+% iremos pegar o comprimento máximo dos vetores, e fazer com que os
+% comprimentos dos outros vetores se adequem ao tamanho maior;
+allSizes = zeros(numberOfCurves,1);
+for i = 1:numberOfCurves
+    allSizes(i) = size(Y(i).plot,2);
+end
+
+maximumSize = max(allSizes);
+
+% Vamos fazer o apêndice de idles no final dos pacotes
+for i = 1:numberOfCurves
+    if size(Y(i).plot,2) == maximumSize
+        theLargerOne = i;
+    else
+        appendixLength = maximumSize-size(Y(i).plot,2);
+        if idle == 0
+            appendix = zeros(appendixLength,1)+(i-1)*2;
+        else
+            appendix = ones(appendixLength,1)+(i-1)*2;
+        end
+        Y(i).plot = [Y(i).plot'; appendix];
+    end
+end
+for i = 1:numberOfCurves
+    X(i).plot = X(theLargerOne).plot;
+end
+
+for i = 1:numberOfCurves 
+    % Esta região do software vai definir quantos bits de um pacote são
+    % representados por um nome, isto é, digamos que tenhamos um pacote dado
+    % por [00001111] e os quatro primeiros bits são chamados de "ZERO" e o
+    % resto chamado de "UM", logo, associaremos o vetor ["ZERO"; "UM"] a [4,4].
+    % Faremos tal associação usando o valor .type de cada canal.
     if strcmp(Signal(i).type,'SM2Arm')
         textArray = java_array('java.lang.String',3);
         textArray(1) = java.lang.String('oneSM2Arm  ');
@@ -47,14 +84,12 @@ for i = 1:numberOfCurves
     end
     X(i).text = getTextCell(X(i).plot, textArray, whereNameStarts);
 end
-%text = getCertainComponents(X, typeOfInput, stringArray);
-
-for i = 1:numberOfCurves
-    Y(i).plot = Y(i).plot + (i-1)*2;
-end
-ylim([-1 2]);
-colors = [ [1 0 0]; [0 1 0]; [0 0 1]; [1 1 0];[1 0 1];[0 1 1];[0 0 0]];
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% xPlot = zeros(maximumSize,numberOfCurves);
+% for i = 1:numberOfCurves
+%     xPlot(:,i) = 
+% end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for i = 1:numberOfCurves
     %ylabel(Signal(i).name, 'position', [-0.005 (i-0.5) 0]);
 %     ylabel('Difference', 'position',[x y z])
